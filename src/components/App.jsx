79be245855e6bda7315925293a49785613b5f077
German Osorio <gosoriocanales@gmail.com>
2024-04-18 10:55:27 -0500

    added navbar and css file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './navbar';
+import '../styles/styles.css';
 
 function App() {
     return (
-        <section>
-            <h1>ReelRemarks</h1>
-        </section>
+        <Router>
+            <section>
+                <section>
+                    <Navbar/>
+                </section>
+                <h1>ReelRemarks</h1>
+            </section>
+        </Router>
     )
 }
 
