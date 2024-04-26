aaa363aadb108c8c5e3225a7377e64ba0300a101
German Osorio <gosoriocanales@gmail.com>
2024-04-26 16:03:48 -0500

    add style to aboout and contact components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function About() {
     return (
-        <section>
-            <h1>About ReelRemarks</h1>
-            <p>Welcome to ReelRemarks - your personal movie review site! Designed for individual users,<br/>
+        <section className='about'>
+            <h1 id='aboutReel'>About ReelRemarks</h1>
+            <p id='aboutPar'>Welcome to ReelRemarks - your personal movie review site! Designed for individual users,<br/>
                 ReelRemarks empowers you to craft and curate your own movie reviews.<br />
                 Dive into the world of cinema, share your insights, and keep track of your cinematic journey with ease.<br />
                 Start your solo movie adventure today with ReelRemarks!</p>
