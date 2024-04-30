04d1b820ae73bd897136478c2716351d8e851fdb
German Osorio <gosoriocanales@gmail.com>
2024-04-30 16:51:41 -0500

    add routes to render

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from "react-router";
 
 function Contact() {
     return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
 
 function Navbar() {
     return (
         <nav id='navbar'>
             <ul>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/about'>About</a></li>
-                <li><a href='/contact'>Contact Us</a></li>
+                <li><NavLink className='link' to='/'>Home</NavLink></li>
+                <li><NavLink className='link' to='/about'>About</NavLink></li>
+                <li><NavLink className='link' to='contact'>Contact Us</NavLink></li>
             </ul>
         </nav>
     );
